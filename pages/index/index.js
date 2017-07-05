341402490eecfe4ee38034731eec1a3dbb4f7da7
高荣伟 <1145318099@qq.com>
2017-07-05 16:22:40 +0800

    首页功能增加

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,130 @@
 // pages/index/index.js
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    indicatorDots: true,
+    vertical: false,
+    autoPlay: true,
+    interval: 3000,
+    duration: 1000,
+    loadingHidden: false,
+    title: "环球精选"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+    app.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+    wx.request({
+      url: 'http://huanqiuxiaozhen.com/wemall/slider/list',
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          images: res.data
+        })
+      }
+    })
+    wx.request({
+      url: 'http://huanqiuxiaozhen.com/wemall/venues/venuesList',
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          venuesItems: res.data.data
+        })
+        setTimeout(function () {
+          that.setData({
+            loadingHidden: true
+          })
+        }, 1500)
+      }
+    })
+    wx.request({
+      url: 'http://huanqiuxiaozhen.com/wemall/goods/choiceList',
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          choiceItems: res.data.data.dataList
+        })
+        setTimeout(function () {
+          that.setData({
+            loadingHidden: true
+          })
+        }, 1500)
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
