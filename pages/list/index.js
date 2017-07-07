ed2fe6dd1dcb24f90a8d2f6fd2cf4d4810b3d706
高荣伟 <1145318099@qq.com>
2017-07-07 10:42:55 +0800

    列表页面

--- pages/list/index.js
+++ pages/list/index.js
@@ -5,62 +5,82 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+    var brand = options.brand;
+    var typeid = options.typeid;
+    wx.request({
+      url: 'http://www.huanqiuxiaozhen.com/wemall/goods/inqGoodsByTypeBrand',
+      method: 'GET',
+      data: {
+        brand: brand,
+        typeid: typeid
+      },
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        var dataList = res.data.data;
+        that.setData({
+          dataList: dataList
+        })
+      }
+
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
