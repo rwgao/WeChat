a6f958993708d75b8982a3420c89f5d6ce812e45
高荣伟 <1145318099@qq.com>
2017-07-17 18:01:02 +0800

    个人中心

--- pages/my/index.js
+++ pages/my/index.js
@@ -1,4 +1,5 @@
 // pages/my/index.js
+var app = getApp()
 Page({
 
   /**
@@ -12,7 +13,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+    app.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
+      })
+    })
   },
 
   /**
