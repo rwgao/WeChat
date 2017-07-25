5e62dd3a0e11a7debc117ff6b1949eec20471a3e
高荣伟 <114518099@qq.com>
2017-07-25 23:17:36 +0800

    购物车列表

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -6,7 +6,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    checkboxItems: [
+      { name: 'standard is dealt for u.', value: '0', checked: true },
+      { name: 'standard is dealicient for u.', value: '1' }
+    ],
   },
 
   /**
@@ -70,6 +73,13 @@ Page({
         },
         success: function(res){
           console.log(res)
+          var cartList = res.data.data.list;
+          for (let i = 0, lenI = cartList.length; i < lenI; i++){
+            for (let j = 0, lenJ = cartList[i].group.lenght; j < lenJ; j++){
+              cartList[i].group[j].checked = true;
+            }
+          }
+          console.log(cartList)
           if (res.data.code == 0) {
             that.setData({
               cartList: res.data.data.list,
@@ -79,6 +89,26 @@ Page({
         }
       })
     }
+  },
+  
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+
+    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false;
+
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true;
+          break;
+        }
+      }
+    }
+
+    this.setData({
+      checkboxItems: checkboxItems
+    });
   },
 
   getCartInfo:function(){
