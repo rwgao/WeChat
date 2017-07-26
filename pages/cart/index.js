b878e84d223e32b03c71b2a9fa950951b996919c
高荣伟 <1145318099@qq.com>
2017-07-26 18:04:46 +0800

    购物车复选操作

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,15 +1,17 @@
 // pages/cart/index.js
 var app = getApp()
+var openid, JSESSIONID1, JSESSIONID2, usropenid;
+openid = usropenid = app.getCookie("usropenid")
+JSESSIONID1 = app.getCookie("JSESSIONID ")
+JSESSIONID2 = app.getCookie("JSESSIONID  ")
+var cookie = "usropenid=" + usropenid + "; JSESSIONID=" + JSESSIONID1 + "; JSESSIONID=" + JSESSIONID2 + ";"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    checkboxItems: [
-      { name: 'standard is dealt for u.', value: '0', checked: true },
-      { name: 'standard is dealicient for u.', value: '1' }
-    ],
+
   },
 
   /**
@@ -17,20 +19,17 @@ Page({
    */
   onLoad: function (options) {
     var that = this;
-    var openid, JSESSIONID1, JSESSIONID2, usropenid;
-    openid = usropenid = app.getCookie("usropenid")
-    JSESSIONID1 = app.getCookie("JSESSIONID ")
-    JSESSIONID2 = app.getCookie("JSESSIONID  ")
-    var cookie = "usropenid=" + 'oI-WduGucLO_WZm7r9Bag7TWdjig' + "; JSESSIONID=" + JSESSIONID1 + "; JSESSIONID=" + JSESSIONID2 + ";"
-    that.getIds(0, cookie )
+    let { data, checked_ids} = that.getIds(0);
+    that.getCartInfo(data, checked_ids, cookie)
   },
 
-  getIds: function (cardfee,cookie){
+  getIds: function (cardfee) {
     var that = this;
     var arr = [];
     var goods = [];
     var openId = app.getCookie("usropenid");
     var goods_info = wx.getStorageInfoSync()
+    let data = {};
     for (let i = 0; i < goods_info.keys.length; i++) {
       if (goods_info.keys[i].indexOf("goods_") != -1) {
         arr.push(goods_info.keys[i].substr(6))
@@ -42,14 +41,14 @@ Page({
       key: 'checked_ids',
       data: checked_ids
     })
-    if (arr.length > 0){
-      for (let j = 0; j < arr.length; j++){
-        var goods_json = JSON.parse(app.getCookie("goods_"+arr[j]));
+    if (arr.length > 0) {
+      for (let j = 0; j < arr.length; j++) {
+        var goods_json = JSON.parse(app.getCookie("goods_" + arr[j]));
         var goodsid = arr[j];
         var carttime = goods_json.timestamp,
-              price = goods_json.ourprice.toFixed(2),
-              amount = goods_json.goodsamount,
-              wareid = goods_json.wareid;
+          price = goods_json.ourprice.toFixed(2),
+          amount = goods_json.goodsamount,
+          wareid = goods_json.wareid;
         goods.push(
           {
             goodsid, carttime, wareid, price, amount
@@ -57,110 +56,141 @@ Page({
         )
       }
       goods = JSON.stringify(goods);
-      wx.request({
-        url: 'http://wx.huanqiuxiaozhen.com/wemall/cart/view',
-        method: 'POST',
-        dataType: 'json',
-        data: {
-          openid: openId,
-          goodsid: ids,
-          cardfee: cardfee,
-          goodsinfo: goods
-        },
-        header: {
-          "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8",
-          'Cookie': cookie + ' checked_ids='+ checked_ids + ';'
-        },
-        success: function(res){
-          console.log(res)
-          var cartList = res.data.data.list;
-          for (let i = 0, lenI = cartList.length; i < lenI; i++){
-            for (let j = 0, lenJ = cartList[i].group.lenght; j < lenJ; j++){
-              cartList[i].group[j].checked = true;
-            }
-          }
-          console.log(cartList)
-          if (res.data.code == 0) {
-            that.setData({
-              cartList: res.data.data.list,
-              stock: res.data.data.qty
-            })
-          }
-        }
-      })
+      data.openId = openId;
+      data.goodsid = ids;
+      data.cardfee = cardfee;
+      data.goodsinfo = goods;
     }
+    return { data, checked_ids}
   },
-  
+
   checkboxChange: function (e) {
     console.log('checkbox发生change事件，携带value值为：', e.detail.value);
-
-    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
-    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-      checkboxItems[i].checked = false;
-
-      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-        if (checkboxItems[i].value == values[j]) {
-          checkboxItems[i].checked = true;
-          break;
+    console.log(e)
+    var cartList = this.data.cartList,
+      values = e.detail.value,
+      key = e.target.dataset["key"];
+    if (key == 'all') {
+      for (let i = 0, lenI = cartList.length; i < lenI; i++) {
+        let list = cartList[i];
+        list.checked = false;
+        for (let k = 0, lenK = list.group.length; k < lenK; k++) {
+          list.group[k].checked = false;
+        }
+        for (let j = 0, lenJ = values.length; j < lenJ; j++) {
+          if (list.id == values[j]) {
+            list.checked = true;
+            for (let l = 0, lenL = list.group.length; l < lenL; l++) {
+              list.group[l].checked = true;
+            }
+          }
+        }
+      }
+    } else {
+      let index = e.target.dataset['index'];
+      for (let i = 0, lenI = cartList[index].group.length; i < lenI; i++) {
+        cartList[index].group[i].checked = false;
+        for (let j = 0, lenJ = values.length; j < lenJ; j++) {
+          if (cartList[index].group[i].goodsid == values[j]) {
+            cartList[index].group[i].checked = true;
+          }
+          if (values.length != cartList[index].group.length) {
+            cartList[index].checked = false;
+          } else {
+            cartList[index].checked = true;
+          }
         }
       }
     }
-
     this.setData({
-      checkboxItems: checkboxItems
+      cartList: cartList
     });
   },
 
-  getCartInfo:function(){
-
+  getCartInfo: function (data, checked_ids, cookie) {
+    var that = this;
+    wx.request({
+      url: 'http://wx.huanqiuxiaozhen.com/wemall/cart/view',
+      method: 'POST',
+      dataType: 'json',
+      data: data,
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        'Cookie': cookie + ' checked_ids=' + checked_ids + ';'
+      },
+      success: function (res) {
+        var cartList = res.data.data.list;
+        for (let i = 0, lenI = cartList.length; i < lenI; i++) {
+          let list = cartList[i];
+          list.checked = true;
+          list.id = i;
+          for (let j = 0, lenJ = list.group.length; j < lenJ; j++) {
+            list.group[j].checked = true;
+          }
+        }
+        if (res.data.code == 0) {
+          that.setData({
+            originData: data,
+            cartList: res.data.data.list,
+            stock: res.data.data.qty
+          })
+        }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    var that = this;
+    let { data, checked_ids } = that.getIds(0);
+    let originData = that.data.originData;
+    if (JSON.stringify(data) != JSON.stringify(originData) && originData != null){
+      that.getCartInfo(data, checked_ids, cookie)
+    }
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
